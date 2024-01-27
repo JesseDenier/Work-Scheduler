71f6ad30a7d00f456b33910a33f34920fa151b62
Jesse <jessecdenier@gmail.com>
2024-01-27 10:40:56 -0600

    Followed DRY principles and consolidated javascript down.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -29,92 +29,27 @@ $(function () {
 });
 
 // Adds a click event to the each save button which saves the corresponding textarea to local storage.
-// TODO: Rewrite code so it is not repetitive.
-// HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
 $(function () {
-  $("#09")
-    .children()
-    .eq(2)
-    .on("click", function () {
-      var scheduledEvent09 = $("#09").children().eq(1).val();
-      localStorage.setItem("scheduledEvent09", scheduledEvent09);
-      console.log(scheduledEvent09);
-    });
-  $("#10")
-    .children()
-    .eq(2)
-    .on("click", function () {
-      var scheduledEvent10 = $("#10").children().eq(1).val();
-      localStorage.setItem("scheduledEvent10", scheduledEvent10);
-      console.log(scheduledEvent10);
-    });
-  $("#11")
-    .children()
-    .eq(2)
-    .on("click", function () {
-      var scheduledEvent11 = $("#11").children().eq(1).val();
-      localStorage.setItem("scheduledEvent11", scheduledEvent11);
-      console.log(scheduledEvent11);
-    });
-  $("#12")
-    .children()
-    .eq(2)
-    .on("click", function () {
-      var scheduledEvent12 = $("#12").children().eq(1).val();
-      localStorage.setItem("scheduledEvent12", scheduledEvent12);
-      console.log(scheduledEvent12);
-    });
-  $("#13")
-    .children()
-    .eq(2)
-    .on("click", function () {
-      var scheduledEvent13 = $("#13").children().eq(1).val();
-      localStorage.setItem("scheduledEvent13", scheduledEvent13);
-      console.log(scheduledEvent13);
-    });
-  $("#14")
-    .children()
-    .eq(2)
-    .on("click", function () {
-      var scheduledEvent14 = $("#14").children().eq(1).val();
-      localStorage.setItem("scheduledEvent14", scheduledEvent14);
-      console.log(scheduledEvent14);
-    });
-  $("#15")
-    .children()
-    .eq(2)
-    .on("click", function () {
-      var scheduledEvent15 = $("#15").children().eq(1).val();
-      localStorage.setItem("scheduledEvent15", scheduledEvent15);
-      console.log(scheduledEvent15);
-    });
-  $("#16")
-    .children()
-    .eq(2)
-    .on("click", function () {
-      var scheduledEvent16 = $("#16").children().eq(1).val();
-      localStorage.setItem("scheduledEvent16", scheduledEvent16);
-      console.log(scheduledEvent16);
-    });
+  $(".saveBtn").on("click", function () {
+    var scheduledEvent = $(this).parent().children().eq(1).val();
+    console.log(scheduledEvent);
+    console.log($(this).parent().attr("id"));
+    localStorage.setItem(
+      "scheduledEvent" + $(this).parent().attr("id"),
+      scheduledEvent
+    );
+  });
 });
 
 // Sets the value of each textarea element to the corresponding localStorage value.
-// TODO: Rewrite code so it is not repetitive.
 $(function () {
-  var scheduledEvent09 = localStorage.getItem("scheduledEvent09");
-  $("#09").children().eq(1).append(scheduledEvent09);
-  var scheduledEvent10 = localStorage.getItem("scheduledEvent10");
-  $("#10").children().eq(1).append(scheduledEvent10);
-  var scheduledEvent11 = localStorage.getItem("scheduledEvent11");
-  $("#11").children().eq(1).append(scheduledEvent11);
-  var scheduledEvent12 = localStorage.getItem("scheduledEvent12");
-  $("#12").children().eq(1).append(scheduledEvent12);
-  var scheduledEvent13 = localStorage.getItem("scheduledEvent13");
-  $("#13").children().eq(1).append(scheduledEvent13);
-  var scheduledEvent14 = localStorage.getItem("scheduledEvent14");
-  $("#14").children().eq(1).append(scheduledEvent14);
-  var scheduledEvent15 = localStorage.getItem("scheduledEvent15");
-  $("#15").children().eq(1).append(scheduledEvent15);
-  var scheduledEvent16 = localStorage.getItem("scheduledEvent16");
-  $("#16").children().eq(1).append(scheduledEvent16);
+  for (var i = 0; i < timeBlockArray.length; i++) {
+    var scheduledEvent = localStorage.getItem(
+      "scheduledEvent" + timeBlockArray[i].id
+    );
+    $("#" + timeBlockArray[i].id)
+      .children()
+      .eq(1)
+      .append(scheduledEvent);
+  }
 });
